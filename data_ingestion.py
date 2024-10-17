57e6a93c198cf6ebdb46a243a07f9bdd0c8ec7c0
RJSINGH64 <rajat.k.singh64@gmail.com>
2024-10-17 14:31:53 +0530

    updated

--- data_ingestion.py
+++ data_ingestion.py
@@ -1,8 +1,7 @@
 import pymongo as pm
 import pandas as pd
-from config import mongo_client #loading mongo url using .env 
 from  dataclasses import dataclass
-
+import os
 
 
 @dataclass
@@ -10,7 +9,8 @@ class DataIngestion:
     
     def initiate_data_ingestion(self):
         try :
-            df =pd.DataFrame(mongo_client["Google"]["apps"].find())  #data ingestion from MngoDB Atlas
+            file_path = os.path.join(os.getcwd() , "dataset\google_data_merged.csv")
+            df =pd.read_csv(file_path) 
             if "_id" in df.columns:   #droping unnecessary column from dataset 
                 df.drop("_id"  , axis=1 , inplace=True)
             return df
@@ -20,6 +20,6 @@ class DataIngestion:
 obj = DataIngestion()
 try :
     df =obj.initiate_data_ingestion()
-    #print(df.head())
+    print(f"\ndata ingestion initiated Sucessfully")
 except Exception as e:
    print(e)   
